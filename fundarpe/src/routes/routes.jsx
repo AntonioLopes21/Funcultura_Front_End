c8a82940ced8f5ae63872ddb129256528215b40f
Antônio de Pádua <antonyejoao@gmail.com>
2025-04-15 15:43:59 -0300

    criando react routes e pages para as telas e implementando usestates

---
+++ fundarpe/src/routes/routes.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from "../pages/HomePage/Home"
+
+const Router = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default Router
