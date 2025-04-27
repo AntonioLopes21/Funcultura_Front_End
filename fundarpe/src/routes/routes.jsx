4b171c2c17ea9cdb81a74b16676874c5ab7b5bd6
Douglas <douglasgomes841@gmail.com>
2025-04-27 20:59:13 -0300

    Tela de login e atualização da home

--- fundarpe/src/routes/routes.jsx
+++ fundarpe/src/routes/routes.jsx
@@ -1,16 +1,18 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import Home from "../pages/HomePage/Home"
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from "../pages/HomePage/Home";
+import LoginPage from "../pages/LoginPage/LoginPage"; // Importa o LoginPage
 
 const Router = () => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home/>}/>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<LoginPage />} /> {/* Adiciona aqui! */}
                 </Routes>
             </BrowserRouter>
         </>
-    )
+    );
 }
 
-export default Router
+export default Router;
