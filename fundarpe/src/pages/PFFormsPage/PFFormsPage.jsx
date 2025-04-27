aa8d56dc6902dc9264796b9b62520262ccf2db6f
Antônio de Pádua <antonyejoao@gmail.com>
2025-04-27 18:30:35 -0300

    finalizando formulário de pessoa física - apenas detalhes visuais

--- fundarpe/src/pages/PFFormsPage/PFFormsPage.jsx
+++ fundarpe/src/pages/PFFormsPage/PFFormsPage.jsx
@@ -9,10 +9,11 @@ function PFFormsPage() {
             <div className="menu_principal">
                 <Header />
                 <div className="main_content">
-                    <h1 className="main_content_title">Formulário de Cadastro de Produtor Cultural</h1>
-                    <h3 className="main_content_subtitle">Informações cadastrais</h3>
+                    <h1 className="main_content_title">Cadastro Pessoa Física</h1>
+                    <hr className="main_content_line" />
                 </div>
                 <div className="formulario">
+                    <h3 className="main_content_subtitle">Informações cadastrais</h3>
 
                     <form>
                         <div className="form_primeira_coluna">
@@ -37,6 +38,77 @@ function PFFormsPage() {
                             </div>
                         </div>
 
+                        {/* seção de documentação */}
+                        <hr className="main_content_line2" />
+
+                        <div className="documentos_para_envio">
+                            <h3 className="main_content_subtitle">Envio de documentos:</h3>
+                        </div>
+
+                        <div className="documentos_para_envio_campos">
+                            <div className="documentos_para_envio_campos_identidade">
+                                <div className="documentos_para_envio_campos_identidade_esquerda">
+                                    <h5>RG, CNH, ou Carteira de Trabalho</h5>
+                                    <p>(Somente arquivos em PDF)</p>
+                                </div>
+                                <div className="documentos_para_envio_campos_identidade_direita">
+                                    <button className="btn_documentos documento_pessoal">Adicionar arquivo</button>
+                                    <p>(Tamanho máximo: 5MB)</p>
+                                </div>
+
+                            </div>
+
+                            <hr className="documentos_para_envio_line"/>
+                            <div className="documentos_para_envio_campos_identidade">
+                                <div className="documentos_para_envio_campos_identidade_esquerda">
+                                    <h5>Comprovante de Residência</h5>
+                                    <p>(Somente arquivos em PDF)</p>
+                                </div>
+                                <div className="documentos_para_envio_campos_identidade_direita">
+                                    <button className="btn_documentos comprovante_residencia">Adicionar arquivo</button>
+                                    <p>(Tamanho máximo: 5MB)</p>
+                                </div>
+
+                            </div>
+
+                            <hr className="documentos_para_envio_line"/><div className="documentos_para_envio_campos_identidade">
+                                <div className="documentos_para_envio_campos_identidade_esquerda">
+                                    <h5>Currículo de atuação cultural</h5>
+                                    <p>(Somente arquivos em PDF)</p>
+                                </div>
+                                <div className="documentos_para_envio_campos_identidade_direita">
+                                    <button className="btn_documentos curriculo_atuacao">Adicionar arquivo</button>
+                                    <p>(Tamanho máximo: 5MB)</p>
+                                </div>
+
+                            </div>
+
+                            <hr className="documentos_para_envio_line"/><div className="documentos_para_envio_campos_identidade">
+                                <div className="documentos_para_envio_campos_identidade_esquerda">
+                                    <h5>Certidão de Regularidade Fiscal</h5>
+                                    <p>(Somente arquivos em PDF)</p>
+                                </div>
+                                <div className="documentos_para_envio_campos_identidade_direita">
+                                    <button className="btn_documentos certidao_regularidade">Adicionar arquivo</button>
+                                    <p>(Tamanho máximo: 5MB)</p>
+                                </div>
+
+                            </div>
+                            <hr className="documentos_para_envio_line"/>
+                            
+                            <div className="documentos_para_envio_campos_identidade">
+                                <div className="documentos_para_envio_campos_identidade_esquerda">
+                                    <h5>Certidão de Prestação de Contas do Funcultura</h5>
+                                    <p>(Somente arquivos em PDF)</p>
+                                </div>
+                                <div className="documentos_para_envio_campos_identidade_direita">
+                                    <button className="btn_documentos certidao_prestacao">Adicionar arquivo</button>
+                                    <p>(Tamanho máximo: 5MB)</p>
+                                </div>
+
+                            </div>
+                            <hr className="documentos_para_envio_line"/>
+                        </div>
                         <div className="btn_cadastro_div">
                             <button type="submit" className="btn_cadastro">Finalizar Cadastro</button>
                         </div>
