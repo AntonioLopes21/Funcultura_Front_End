6f4878fe5320725dbb7a3f87dd855e136c5ac0ea
Antônio de Pádua <antonyejoao@gmail.com>
2025-06-02 11:32:51 -0300

    fix - pastas testes deletadas

--- fundarpe/src/assets/Images.jsx
+++ fundarpe/src/assets/Images.jsx
@@ -1,14 +1,14 @@
-import ImagemNav from "../assets/images/funculturalLogo.svg";
-import LogoCPC from "../assets/images/LogoCPC.png"
-import ImagemFooter from "../assets/images/imagem_footer.png"
-import logoFacebok from "../assets/images/facebook_icon.png"
-import logoFlickr from "../assets/images/flickr_icon.png"
-import logoTwitter from "../assets/images/twitter_icon.png"
-import logoYoutube from "../assets/images/youtube_Icon.png"
-import FunculturaLogo from "../assets/images/FUNCULTURA_logo.png"
-import FundarpeLogo from "../assets/images/FUNDARPE_logo.png"
-import GovLogo from "../assets/images/GOV_logo.png"
-import FundarpeLogos from "../assets/images/fundarpe_logos.svg"
+import ImagemNav from "./images/funculturalLogo.svg";
+import LogoCPC from "./images/LogoCPC.png"
+import ImagemFooter from "./images/imagem_footer.png"
+import logoFacebok from "./images/facebook_icon.png"
+import logoFlickr from "./images/flickr_icon.png"
+import logoTwitter from "./images/twitter_icon.png"
+import logoYoutube from "./images/youtube_Icon.png"
+import FunculturaLogo from "./images/FUNCULTURA_logo.png"
+import FundarpeLogo from "./images/FUNDARPE_logo.png"
+import GovLogo from "./images/GOV_logo.png"
+import FundarpeLogos from "./images/fundarpe_logos.svg"
 
 const ImagesProject = {
     ImagemNav,
@@ -22,8 +22,6 @@ const ImagesProject = {
     FundarpeLogo,
     GovLogo,
     FundarpeLogos,
-
 }
-
 
 export { ImagesProject }
